3ca6dcd4e71321a60bbb1a105cf27c7fc1c136ff
jnaoroji <jennynaoroji@gmail.com>
2023-03-29 01:25:18 +1000

    if conditions added to all prompts

--- Assets/script.js
+++ Assets/script.js
@@ -15,24 +15,46 @@ function generatePassword() {
   var passwordLength = window.prompt("Choose a length between 8 - 128 characters");
   //var index = Math.floor(Math.random() * passwordLength);
   if (!passwordLength) {
+    alert("This field is required!")
     return;
   }
+  else if (passwordLength < 8 || passwordLength > 128){
+  alert("Invalid Choice! Try again to choose a length between 8 - 128 characters")
+  return;
+  }
+  else (passwordLength >= 8 && passwordLength >= 128);{
+    console.log(passwordLength);
+  }
+    
   var passwordLowercase = window.confirm("Would you like to include lowercase letters?");
   
   if (!passwordLowercase) {
-    return;
+    console.log(!passwordLowercase);
   }
+  else if (passwordLowercase){
+    console.log(passwordLowercase);
+  }
+
   var passwordUppercase = window.confirm("Would you like to include Uppercase letters?");
   if (!passwordUppercase) {
-    return;
+    console.log(!passwordUppercase);
+  }
+  else if (passwordUppercase){
+    console.log(passwordUppercase);
   }
   var passwordNumeric = window.confirm("Would you like to include Numbers?");
   if (!passwordNumeric) {
-    return;
+    console.log(!passwordNumeric);
+  }
+  else if (passwordNumeric) {
+    console.log(passwordNumeric);
   }
   var passwordSpecial = window.confirm("Would you like to include Special Characters?");
   if (!passwordSpecial) {
-    return;
+    console.log(!passwordSpecial);
+  }
+  else if (passwordSpecial) {
+    console.log(passwordSpecial);
   }
 }
 // Write password to the #password input
